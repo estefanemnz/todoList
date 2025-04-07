dfb1d0ae30c8fc5db2606fe08ae88af73707f582
estefanemnz <estefanemenez@gmail.com>
2025-04-06 21:56:15 -0400

    estilização do painel de controle

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import { v4 as uuidv4 } from 'uuid'
-
 import TodoControls from '@/components/TodoControls/TodoControls'
 import TaskList from '@/components/TaskList/TaskList'
-import Modal from '@/components/Modal/Modal'
+
 function App() {
 
   const [Tasks, setTasks] = useState([
-    {title: "Lavar banheiro",
+    {title: "Tarefa 1",
       id: uuidv4()
     },
     {title: "Estudar POO",
@@ -17,14 +16,14 @@ function App() {
     {title: "Fazer conciliação",
       id: uuidv4()
     },
-    {title: "Lavar roupas",
+    {title: "Terminar CSS",
       id: uuidv4()
     }
   ]);
 
   return (
     <>
-      <div >
+      <div className='container'>
         <TodoControls/>
         <TaskList tasks={Tasks} setTasks={setTasks}/>
       </div>
--- src/components/TodoControls/TodoControls.jsx
+++ src/components/TodoControls/TodoControls.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
 import ColorScheme from '@/assets/ColorScheme.svg'
 import SearchIcon from '@/assets/SearchIcon.svg'
+import styles from './TodoControls.module.css'
 
 function TodoControls() {
   return (
-    <div>
-        <strong>Lista de Tarefas</strong>
-        <div>
-          <input placeholder='Buscar tarefa'/>
-          <img src={SearchIcon}/>
+    <div className={styles.panelControl}>
+      <h1>Lista de Tarefas</h1>
+      <div className={styles.container}>
+        <div className={styles.search}>
+          <input placeholder='Buscar tarefa...' />
+          <button className={styles.searchButton}>
+            <img src={SearchIcon} />
+          </button>
         </div>
-        <div>
-          <select>
+        <div className={styles.filter}>
+          <select className={styles.selected}>
             <option value='all'>Todas</option>
             <option value='completed'>Completas</option>
             <option value='incomplete'>Incompletas</option>
           </select>
         </div>
         <button>
-          <img src={ColorScheme}/>
+          <img src={ColorScheme} />
         </button>
+      </div>
     </div>
   )
 }
