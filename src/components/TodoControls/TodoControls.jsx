927ae0e019e2027601cfe069c230d46b7e9fb09f
estefanemnz <estefanemenez@gmail.com>
2025-04-08 22:45:04 -0400

    react select adicionado

--- src/components/TodoControls/TodoControls.jsx
+++ src/components/TodoControls/TodoControls.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
+import Select from 'react-select'
+
 import ColorScheme from '@/assets/ColorScheme.svg'
 import SearchIcon from '@/assets/SearchIcon.svg'
+
 import styles from './TodoControls.module.css'
 
 function TodoControls() {
+
+  const options = [
+    { value: 'todas', label: 'Todas'},
+    { value: 'incompletas', label: 'Incompletas'},
+    { value: 'completas', label: 'Completas'}
+  ]
+
+  const stylesSelect = {
+    control: (base, state) => ({
+      ...base,
+      height: '100%',
+      fontWeight: 'bold',
+      fontSize: '1rem',
+      borderRadius: '8px',
+      color: 'white',
+      backgroundColor:' #534CC2',
+      cursor: 'pointer',
+      padding: '10px',
+      boxShadow: state.isFocused ? ' 0 0 0 2px #6C63FF' : 'none',
+      outline: 'none',
+    }),
+
+    option: (base, state) => ({
+      ...base,
+      cursor: 'pointer',
+      backgroundColor:' #f7f7f7',
+      color:' #6c63ff',
+      fontWeight: 'bold',
+      borderRadius: '8px',
+    }),
+
+    singleValue: (base) => ({
+      ...base,
+      color: 'white'
+    }),
+
+    menu: (base) => ({
+      ...base,
+      borderRadius: '8px',
+      border: '1px solid #6C63FF',
+
+    })
+  };
+
   return (
     <div className={styles.containerPanelControl}>
       <h1>Lista de Tarefas</h1>
@@ -15,11 +62,13 @@ function TodoControls() {
           </button>
         </div>
         <div className={styles.filter}>
-          <select className={styles.selected}>
-            <option value='all' className={styles.option}>Todas</option>
-            <option value='completed'  className={styles.option}>Completas</option>
-            <option value='incomplete'  className={styles.option}>Incompletas</option>
-          </select>
+          <Select 
+            classNamePrefix="select"
+            className={styles.selectContainer}
+            options={options}
+            defaultValue={options[0]}
+            styles={stylesSelect}
+          />
         </div>
         <button className={styles.colorButton}>
           <img src={ColorScheme} />
