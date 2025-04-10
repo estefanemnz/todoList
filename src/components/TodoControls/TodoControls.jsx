854b1882adcb6b8faef4ac8880a39f5dd7df658c
estefanemnz <estefanemenez@gmail.com>
2025-04-10 17:10:56 -0300

    att header

--- src/components/TodoControls/TodoControls.jsx
+++ src/components/TodoControls/TodoControls.jsx
@@ -17,25 +17,37 @@ function TodoControls() {
   const stylesSelect = {
     control: (base, state) => ({
       ...base,
+      minWidth: '150px',
       height: '100%',
       fontWeight: 'bold',
       fontSize: '1rem',
       borderRadius: '8px',
       color: 'white',
-      backgroundColor:' #534CC2',
+      backgroundColor:' #6C63FF',
       cursor: 'pointer',
-      padding: '10px',
-      boxShadow: state.isFocused ? ' 0 0 0 2px #6C63FF' : 'none',
+      boxShadow: '0 0 1.5px 2px rgba(108, 99, 255, 0.3);',
       outline: 'none',
+      border: '2px solid #6C63FF',
+      '&:hover': {
+        boxShadow: '0 0 12px 2px rgba(108, 99, 255, 0.4);'
+      },
+      ...(state.isFocused && {
+        backgroundColor: '#534CC2',
+        border: '2px solid #6c63ff',
+        boxShadow: 'box-shadow: 0 0 0 2px #6C63FF;',
+        outline: 'none',
+      })
     }),
 
-    option: (base, state) => ({
+    option: (base) => ({
       ...base,
       cursor: 'pointer',
       backgroundColor:' #f7f7f7',
-      color:' #6c63ff',
+      color:' #6C63FF',
       fontWeight: 'bold',
-      borderRadius: '8px',
+      '&:hover': {
+        backgroundColor: '#DBD9F9',
+      }
     }),
 
     singleValue: (base) => ({
@@ -48,7 +60,9 @@ function TodoControls() {
       borderRadius: '8px',
       border: '1px solid #6C63FF',
 
-    })
+    }),
+
+
   };
 
   return (
@@ -68,6 +82,7 @@ function TodoControls() {
             options={options}
             defaultValue={options[0]}
             styles={stylesSelect}
+            blurInputOnSelect
           />
         </div>
         <button className={styles.colorButton}>
